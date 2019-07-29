6ea9065b9820e379c164c46227d373ad7daf09a1
Yesidh <tim@timrich.org>
2019-07-29 16:42:59 -0500

    #Fix: changing in the line 16 the command exit (1) by return (-1)

--- _printf.c
+++ _printf.c
@@ -9,13 +9,11 @@ int _printf(const char *format, ...)
 	unsigned int i = 0, j = 0, len = 0;
 	va_list char_list;
 	c_f selector[] = { {"c", f_character},
-		{"s", f_string},
-		{NULL, NULL}
-	};
+		{"s", f_string}, {NULL, NULL} };
 
 	va_start(char_list, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		exit(1);
+		return (-1);
 	for (; format && format[i]; i++)
 	{
 		if (format[i] != '%')
