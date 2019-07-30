ccd8ee299721078e69b59621db0ee55b8e800c51
julgachancipa <julianaga01@gmail.com>
2019-07-30 23:54:50 +0000

    #Fix: adding %r case to the array

--- _printf.c
+++ _printf.c
@@ -8,8 +8,10 @@ int _printf(const char *format, ...)
 {
 	unsigned int i = 0, j, flag = 0, len = 0;
 	va_list char_list;
-	c_f selector[] = { {"c", f_character}, {"s", f_string}, {"b", f_binary},
-			   {"d", f_integer}, {"i", f_integer}, {NULL, NULL} };
+	c_f selector[] = { {"c", f_character}, {"s", f_string},
+			   {"b", f_binary}, {"r", f_reverse},
+			   {"d", f_integer}, {"i", f_integer},
+			   {NULL, NULL} };
 
 	va_start(char_list, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
@@ -25,12 +27,12 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			for (j = 0; j < 5; j++)
+			for (j = 0; j < 6; j++)
 			{
 				if (format[i + 1] == selector[j].sp_char[0])
 				{
 					len += selector[j].f(char_list);
-					i ++;
+					i++;
 					flag++;
 					break;
 				}
